e8fe7c133d75fba35f86c58b57e23561f0627d0e
Chase <Gcrowder93@gmail.com>
2021-09-29 11:04:31 -0700

    console log notes+remember log shortcut CLG

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
-// import functions and grab DOM elements
+/*console.log("hi");
 
-// initialize global state
+const PI = 3.1415926;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+console.log(PI);
+console.log('the are of a circle with radius 2 is: ${PI * 2 * 2}');
+
+let firstName = 'chase';
+firstName = 'chase';
+
+/* only use ${} with backtics ` (above tab) */
+
+//console.log(`my name is ${firstName}`)
+
+/* vaiable rules - variable names contain a-z
+A-Z
+$ _
+0-9
+cannot start with number */
+
+//console.log(4 + '2')
+//console.log("4" + "2");
+
+/*vs*/
+
+//console.log(4 + 2);
+//let string42 = '42'; /* this line becomes '6' */
+
+//tells you what your date type is
+//console.log(typeof string42) 
+
+
+// arrays
+
+let myList = [1, 2, 3, 4];
+console.log(myList);
+let nums = [1, 2, 3, 4];
+let names = ['julie', 'benny', 'tis', 'dan'];
+let mixed = [1, 'julie', 2, 'benny'];
+console.log(nums);
+console.log(names);
+console.log(mixed);
+
+let colors = ['red', 'white', 'blue'];
+console.log(colors[0]);
+console.log(colors[2]);
+
+const theColorBlue = colors[2];
+console.log(theColorBlue);
+
+colors[2] = 'purple';
+console.log(colors);
+
+console.log(theColorBlue);
+colors.push('orange'); //adds colors
+console.log(colors);
+
+
+colors.pop(); //removes last element on list
+console.log(colors);
+
+colors.shift(); //removes first element on list
+console.log(colors);
